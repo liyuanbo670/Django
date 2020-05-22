7b2a1c1e811c1963637b8add1bd6fd86a05339a6
liyuanbo670 <64070929+liyuanbo670@users.noreply.github.com>
2020-05-22 13:37:37 +0800

    Create setting.py

---
+++ untitled/setting.py
@@ -0,0 +1,11 @@
+# 配置数据库
+DATABASES = {
+    "default":{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'db1',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':3306
+    }
+}
